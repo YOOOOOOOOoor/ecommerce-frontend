731ebda6d837f9c918eaabf39abca5b45e9a6f57
Yor <164674888+YOOOOOOOOoor@users.noreply.github.com>
2026-06-27 15:49:54 +0300

    Update VaE.jsx

--- src/pages/seller/VaE.jsx
+++ src/pages/seller/VaE.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import API from "../../Api/api.js";
 import toast from "react-hot-toast"; // <-- import toast
 import Add from "./Add.jsx";
+import ImageWithSkeleton from "../../components/ImageWithSkeleton.jsx";
 import { useNavigate } from "react-router-dom";
 
 const VaE = ({ refershKey, user }) => {
@@ -97,7 +98,7 @@ const VaE = ({ refershKey, user }) => {
             <div key={product.id} className="product">
               <p className="Category">{product.category}</p>
               <div className="IMG">
-                <img src={product.image} alt={product.name} />
+               <ImageWithSkeleton src={product.image} alt={product.name} />
               </div>
               <div className="Other_parts">
                 <div>
